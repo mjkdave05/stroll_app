e5d65daf7fff9834eb70846d2f22a454938ef970
Majekodunmi David <majekodunmidavid05@gmail.com>
2025-01-19 00:06:29 +0100

    mvvm file structure set

---
+++ lib/presentation/stroll_bonfire_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StrollBonfirePage extends StatelessWidget {
+  const StrollBonfirePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
