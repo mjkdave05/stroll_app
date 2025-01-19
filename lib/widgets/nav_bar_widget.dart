8c14f3420806e7502dd432bf666fe063ab5c6213
Majekodunmi David <majekodunmidavid05@gmail.com>
2025-01-19 21:01:14 +0100

    nav bar added and project completed

--- lib/widgets/nav_bar_widget.dart
+++ lib/widgets/nav_bar_widget.dart
@@ -7,35 +7,67 @@ class NavigationBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: const Color(0xFF0F1115), // Background color of the bar
+      selectedItemColor: const Color(0xFF9C88FF), // Selected icon color
+      unselectedItemColor: Colors.grey, // Unselected icon color
+      showSelectedLabels: false, // Hides labels when selected
+      showUnselectedLabels: false, // Hides labels when not selected
       items: [
         BottomNavigationBarItem(
-          backgroundColor: Color(0xFF0F1115),
           icon: SvgPicture.asset(
-            'assets/icons/Card.svg',
-            width: 24,
-            height: 24,
-            color: Colors.black,
+            'assets/images/Card.svg',
+            width: 40,
+            height: 40,
+            colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+          ),
+          activeIcon: SvgPicture.asset(
+            'assets/images/Card.svg',
+            width: 40,
+            height: 40,
+            colorFilter: const ColorFilter.mode(Color(0xFF9C88FF), BlendMode.srcIn),
           ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
           icon: SvgPicture.asset(
-            'assets/icons/bonfire.svg',
-            width: 24,
-            height: 24,
+            'assets/images/bonfire.svg',
+            width: 40,
+            height: 40,
+            colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+          ),
+          activeIcon: SvgPicture.asset(
+            'assets/images/bonfire.svg',
+            width: 40,
+            height: 40,
+            colorFilter: const ColorFilter.mode(Color(0xFF9C88FF), BlendMode.srcIn),
           ),
           label: 'Bonfire',
         ),
         BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/icons/message.svg',
-            width: 24,
-            height: 24,
+          icon: Image.asset(
+            'assets/images/Chat.png', // PNG for inactive state
+            width: 40,
+            height: 40,
+          ),
+          activeIcon: Image.asset(
+            'assets/images/Chat.png', // PNG for active state
+            width: 40,
+            height: 40,
           ),
           label: 'Messages',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline), // You can replace this with another SVG if needed.
+          icon: const Icon(
+            Icons.person_outline,
+            color: Colors.grey,
+            size: 35,
+          ),
+          activeIcon: const Icon(
+            Icons.person,
+            color: Color(0xFF9C88FF),
+            size: 35,
+          ),
           label: 'Profile',
         ),
       ],
